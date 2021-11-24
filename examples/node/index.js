5631ed2010b9fccfa8ab2a2f405150f6f67a16a7
Horace Williams <horace.d.williams@gmail.com>
2021-11-24 09:36:18 -0800

    Turn node js index example into a runnable converter

--- examples/node/index.js
+++ examples/node/index.js
@@ -3,41 +3,47 @@
 const geojson = require("flatgeobuf/lib/cjs/geojson")
 const fs = require('fs');
 
-const expected = {
-    type: 'FeatureCollection',
-    features: [{
-        type: 'Feature',
-        geometry: {
-            type: 'Point',
-            coordinates: [0, 0]
-        }
-    }]
-}
+// const expected = {
+//     type: 'FeatureCollection',
+//     features: [{
+//         type: 'Feature',
+//         geometry: {
+//             type: 'Point',
+//             coordinates: [0, 0]
+//         }
+//     }]
+// }
 
-console.log("Input GeoJSON:")
-console.log(JSON.stringify(expected, undefined, 1))
+// console.log("Input GeoJSON:")
+// console.log(JSON.stringify(expected, undefined, 1))
 
-const flatgeobuf = geojson.serialize(expected)
-console.log(`Serialized input GeoJson into FlatGeobuf (${flatgeobuf.length} bytes)`)
+// const flatgeobuf = geojson.serialize(expected)
+// console.log(`Serialized input GeoJson into FlatGeobuf (${flatgeobuf.length} bytes)`)
 
-console.log('writing to output /tmp/node.fgb');
-fs.writeFile('/tmp/node.fgb', Buffer.from(flatgeobuf),(err) => {
-  if (err) throw err;
-  console.log('The file has been saved!');
+// console.log('writing to output /tmp/node.fgb');
+// fs.writeFile('/tmp/node.fgb', Buffer.from(flatgeobuf),(err) => {
+//   if (err) throw err;
+//   console.log('The file has been saved!');
+// });
+
+
+// const actual = geojson.deserialize(flatgeobuf)
+
+// console.log('FlatGeobuf deserialized back into GeoJSON:')
+// console.log(JSON.stringify(actual, undefined, 1))
+
+const file = process.argv[2];
+console.error(file);
+
+const geoq = fs.readFileSync(file);
+const res = geojson.deserialize(geoq);
+res['features'].forEach((f) => {
+  if (f['properties'] === undefined) {
+    f['properties'] = {};
+  }
 });
 
-
-const actual = geojson.deserialize(flatgeobuf)
-
-console.log('FlatGeobuf deserialized back into GeoJSON:')
-console.log(JSON.stringify(actual, undefined, 1))
-
-
-const geoq = fs.readFileSync('/tmp/test.fgb');
-console.log('/tmp/test.fgb:');
-console.log(geoq);
-
-// const geoqDeSer = geojson.deserialize(geoq);
+const geoqDeSer = JSON.stringify(res);
 // console.log('geoq deserialized:');
-
-// console.log(geoqDeSer);
+// console.log(geoq);
+console.log(geoqDeSer);
